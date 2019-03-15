95ce612ba7d9189226150b6e20fb75b35a1fe441
Robert Wildman <robertwildman4@gmail.com>
2019-03-15 10:00:43 +0000

    Adding stations

--- public/JS/citybikes.js
+++ public/JS/citybikes.js
@@ -1,5 +1,5 @@
 var mymap;
-var markergroup;
+var markergroup,stationgroup;
 var networkids;
 makemap();
 function makemap()
@@ -15,22 +15,50 @@ function makemap()
 }
 function displaynetworks()
 {
-    //{"company":["Nextbike GmbH"],"href":"/v2/networks/norisbike-nurnberg","id":"norisbike-nurnberg","location":{"city":"N\u00fcrnberg","country":"DE","latitude":49.4479,"longitude":11.0814},"name":"NorisBike"}
-    $.ajax({
+     $.ajax({
         url: "https://api.citybik.es/v2/networks",
         type: "get", //send it through get method
         success: function(response) {
-            markergroup = L.layerGroup();
+            markergroup = L.markerClusterGroup();
             $.each(response.networks, function(index, value) {
                //Add markers to the maps 
                 var city = value.location;
                 var marker = L.marker([city.latitude, city.longitude]).addTo(markergroup);
-                marker.bindPopup("<b>"+value.name+"</b><p> City: " + city.city + " <br> ID: "+value.id+"</p>").openPopup();
+                marker.bindPopup("<b>"+value.name+"</b><p> City: " + city.city + " <br> <a onclick='displaystations(\""+value.id+"\")' href='javascript:void(0);'>View Network </a> </p>").openPopup();
             }); 
             markergroup.addTo(mymap);
         },
         error: function(xhr) {
           //Do Something to handle error
+        }
+    });
+
+
+
+}
+
+function displaystations(id)
+{
+    if(stationgroup != undefined)
+    {
+        mymap.removeLayer(stationgroup);
+    }
+    $.ajax({
+        url: "https://api.citybik.es/v2/networks/"+id,
+        type: "get", //send it through get method
+        success: function(response) {
+            var network = response.network;
+            stationgroup = L.markerClusterGroup();
+            $.each(network.stations, function(index, value) {
+               //Add markers to the maps
+               console.log(value); 
+                var marker = L.marker([value.latitude, value.longitude]).addTo(stationgroup);
+                marker.bindPopup("<b>"+value.name+"</b><p> Free Bike: " + value.free_bikes + "<br> Empty Slots: "+value.empty_slots+"</p>").openPopup();
+            }); 
+            stationgroup.addTo(mymap);
+        },
+        error: function(xhr) {
+          console.log(xhr);
         }
     });
 }
