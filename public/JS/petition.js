c2da1f9b1417cd4aa63709c964b32466daf1f664
Robert Wildman <robertwildman4@gmail.com>
2019-03-26 14:46:13 +0000

    Adding heatmap

--- public/JS/petition.js
+++ public/JS/petition.js
@@ -1,13 +1,46 @@
 var mymap;
-var petitionData = {};
+var petitionData = [];
 var geo;
-petitionData.name = "Robert";
-console.log(petitionData.name);
+$( document ).ready(function() {
+//Get Petition data from Gov 
+    $.ajax({
+        url: "https://petition.parliament.uk/petitions/243319.json",
+        type: "get", //send it through get method
+        success: function(response) {
+            //Looping through all constituency in the JSON file
+            $.each(response.data.attributes.signatures_by_constituency, function(index, value) {
+                //Adding it to the petition array
+                petitionData.push(value);
+            }); 
+            //Call loading in the map
+            loadmap();
+        },
+        error: function(xhr) {
+        //Do Something to handle error
+        }
+    });
+//Styling the map
+});
+
+
+
+
+
+
+
 
 function style(feature) {
+    var colornum = 1;
+    $.each(petitionData, function(index, value) {
+        if(value.ons_code == feature.properties.PCON13CD)
+        {
+            colornum = value.signature_count;
+    
+        }
+    });
     return {
-        fillColor: getColor(670),
-        weight: 2,
+        fillColor: getColor(colornum),
+        weight: 0.5,
         opacity: 1,
         color: 'white',
         dashArray: '3',
@@ -15,20 +48,38 @@ function style(feature) {
     };
 }
 function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
+    var max = gethigh()/8;
+    return d > [max*7] ? '#800026' :
+           d > [max*6]  ? '#BD0026' :
+           d > [max*5]  ? '#E31A1C' :
+           d > [max*4]  ? '#FC4E2A' :
+           d > [max*3]   ? '#FD8D3C' :
+           d > [max*2]   ? '#FEB24C' :
+           d > [max*1]   ? '#FED976' :
                       '#FFEDA0';
+}
+function gethigh()
+{
+    var high = 1;
+    $.each(petitionData, function(index, value) {
+        if(value.signature_count > high)
+        {
+            high = value.signature_count;
+        }
+    });
+    return high;
 }
 function highlightFeature(e) {
     var layer = e.target;
-    $("#con").text(layer.feature.properties.PCON13NM);
+    $.each(petitionData, function(index, value) {
+        if(value.ons_code == layer.feature.properties.PCON13CD)
+        {
+            $("#con").text(value.mp + ", " + value.name + " , " + value.signature_count);
+    
+        }
+    });
     layer.setStyle({
-        weight: 5,
+        weight: 2,
         color: '#666',
         dashArray: '',
         fillOpacity: 0.7
@@ -51,9 +102,21 @@ function onEachFeature(feature, layer) {
         click: zoomToFeature
     });
 }
+function loadmap()
+{
+    mymap = L.map('mapid').setView([54.527457, -2.911369], 5);
+    $.getJSON( "topo_wpc.json", function(topology) {
+        geo = L.geoJSON(topojson.feature(topology, topology.objects.wpc), {style: style,onEachFeature: onEachFeature}).addTo(mymap);
+    });
+}
+function getPetitionData(code)
+{
+    $.each(petitionData, function(index, value) {
+        if(value.ons_code == code)
+        {
+            return value;
+        }
+    });
+}
 
 
-mymap = L.map('mapid').setView([54.527457, -2.911369], 5);
-$.getJSON( "topo_wpc.json", function(topology) {
-    geo = L.geoJSON(topojson.feature(topology, topology.objects.wpc), {style: style,onEachFeature: onEachFeature}).addTo(mymap);
-});
