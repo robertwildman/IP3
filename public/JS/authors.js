9ed2a53ade637b99e0e8637ba945fd0d3ebe472e
LewisBohme <47980169+LewisBohme@users.noreply.github.com>
2019-04-12 19:03:45 +0100

    CSS work-Overviewpage & Authors
    Fixed all icons in the overview splash page, changed authors to flex, gave buttons borders in overview & tidied up

--- public/JS/authors.js
+++ public/JS/authors.js
@@ -2,38 +2,32 @@ $(function(){
 
   let users = [
     {
-      firstname: "janne",
-      lastname: "antila",
+      Name: "Janne Antila",
       picture: "../IMAGES/muhku.jpg",
       description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
     },
     {
-      firstname: "benjamin",
-      lastname: "hollywood",
+      Name: "Benjamin Hollywood",
       picture: "../IMAGES/muhku.jpg",
       description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
     },
     {
-      firstname: "robert",
-      lastname: "wildman",
+      Name: "Robert Wildman",
       picture: "../IMAGES/muhku.jpg",
       description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
     },
     {
-      firstname: "lewis",
-      lastname: "bohme",
+      Name: "Lewis Bohme",
       picture: "../IMAGES/muhku.jpg",
       description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
     },
     {
-      firstname: "adrian",
-      lastname: "hollywood",
+      Name: "Adrian McAulay",
       picture: "../IMAGES/muhku.jpg",
       description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
     },
     {
-      firstname: "stephen",
-      lastname: "wrath",
+      Name: "Stephen Wrath",
       picture: "../IMAGES/muhku.jpg",
       description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?"
     }
@@ -45,9 +39,9 @@ $(function(){
     $('#profiles').append(`
                             <div id="profile${index}" class="profile">
                               <div class="imageContainer">
-                                <img src=${user.picture} alt="${user.firstname} class="profilepicture" width="150" height="220" >
+                                <img src=${user.picture} alt="${user.Name} class="profilepicture" width="150" height="220" >
                               </div>
-                              <h3>${user.firstname}</h3>
+                              <h2>${user.Name}</h2>
                               <p>${user.description}</p>
                             </div>
                           `)
