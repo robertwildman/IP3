8168b523f1d5d54a45c579c161af309d9ff2a652
Robert Wildman <robertwildman4@gmail.com>
2019-04-14 23:40:46 +0100

    Writing datasets tuts

--- public/JS/datasets.js
+++ public/JS/datasets.js
@@ -27,4 +27,39 @@ $(document).ready(function(){
         $("#petitiondiv").hide();
         $("#postcodediv").show();
     }); 
+    
+    $("#di1").hide();
+        $("#dt1").hide();
+        $("#do1").hide();
+        $("#du1").hide();
+    
+
 });
+function iclick(id)
+{
+    $("#di"+id).show();
+    $("#dt"+id).hide();
+    $("#do"+id).hide();
+    $("#du"+id).hide();
+}
+function tclick(id)
+{
+    $("#di"+id).hide();
+    $("#dt"+id).show();
+    $("#do"+id).hide();
+    $("#du"+id).hide();
+}
+function oclick(id)
+{
+    $("#di"+id).hide();
+    $("#dt"+id).hide();
+    $("#do"+id).show();
+    $("#du"+id).hide();
+}
+function uclick(id)
+{
+    $("#di"+id).hide();
+    $("#dt"+id).hide();
+    $("#du"+id).show();
+    $("#do"+id).hide();
+}
