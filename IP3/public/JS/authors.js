6b045b8f7da665d9a6f56ac480d414129f04aadc
LewisBohme <47980169+LewisBohme@users.noreply.github.com>
2019-04-23 22:43:59 +0100

    CSS -23/04/19
    Change to Authors, Crypto & Lat Long Info

--- IP3/public/JS/authors.js
+++ IP3/public/JS/authors.js
@@ -3,33 +3,33 @@ $(function(){
   let users = [
     {
       Name: "Janne Antila",
-      picture: "../IMAGES/TEMP-REMOVE/User.png",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
+      picture: "../IMAGES/Janne.jpg",
+      description: "ITMB 3rd Year <br>Janne was the second developer for the project. He was responsible for developing most of the static pages as well as the weather and earthquake pages"
     },
     {
       Name: "Benjamin Hollywood",
-      picture: "../IMAGES/TEMP-REMOVE/User.png",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
+      picture: "../IMAGES/Benjamin.jpeg",
+      description: "ITMB 3rd Year <br>Benjamin was the mastermind behind organising documentation for the group. He was responsible for designing the overall appearance of the documentation and the poster deliverable"
     },
     {
       Name: "Robert Wildman",
-      picture: "../IMAGES/TEMP-REMOVE/User.png",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
+      picture: "../IMAGES/Robert.png",
+      description: "Computing 3rd Year <br>Robert was the technical lead for the group. Robert decided what technology stack would be used for the project and how pages would be implemented"
     },
     {
       Name: "Lewis Bohme",
-      picture: "../IMAGES/TEMP-REMOVE/User.png",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
+      picture: "../IMAGES/Lewis.jpeg",
+      description: "Computing 3rd Year <br>Lewis worked on the project as a designer with Adrian. His main task was designing the navigation & several pages for the application "
     },
     {
       Name: "Adrian McAulay",
-      picture: "../IMAGES/TEMP-REMOVE/User.png",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?<"
+      picture: "../IMAGES/Adrian.png",
+      description: "Computing 3rd Year <br>Adrian was the second designer of the project. He worked on the CSS extensively with Lewis to produce a responsive UI for the project"
     },
     {
       Name: "Stephen Wrath",
-      picture: "../IMAGES/TEMP-REMOVE/User.png",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, ea suscipit labore, ratione omnis voluptatum maxime nisi adipisci eaque, doloremque deserunt similique? Laudantium cum unde aperiam rerum obcaecati, eveniet aliquam itaque accusamus suscipit ex neque reiciendis deserunt explicabo iure eaque nesciunt autem quis sit animi optio facilis ipsum? Maiores et quia dolorum repellat, nulla debitis minima voluptate, sit, voluptates aliquid beatae quas itaque eveniet! Assumenda, dolor quaerat sunt corporis obcaecati, nesciunt error provident qui, magni cumque mollitia consequatur nemo exercitationem?"
+      picture: "../IMAGES/Stephen.png",
+      description: "Computing 3rd Year <br>Stephen worked mainly on the documentation of the project. He was responsible for coordinating the groups efforts for the deliverables of the module"
     }
   ];
   /**
@@ -39,7 +39,7 @@ $(function(){
     $('#profiles').append(`
                             <div id="profile${index}" class="profile">
                               <div class="imageContainer">
-                                <img src=${user.picture} alt="${user.Name} class="profilepicture" width="150" height="220" >
+                                <img src=${user.picture} alt="${user.Name} class="profilepicture" width="220" height="220" >
                               </div>
                               <h2>${user.Name}</h2>
                               <p>${user.description}</p>
